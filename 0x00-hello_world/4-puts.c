91097286f49c7679e568ba72e4e645079c0d343e
TebogoMasete <Tebogomasete16@gmail.com>
2022-06-30 15:08:58 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,22 +1,17 @@
 #!/bin/bash
 #include <stdio.h>
 
+/
 
-
-/**
- *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
+ * main - Entry point
+ * Return: Always 0 (Success)
+ * /
 
 int main(void)
 
 {
-
-	    return (0);
+puts("\"Programming is like building s multilingual puzzzle")
+	    
+ return (0);
 
 }
