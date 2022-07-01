0aefa7602f8fecdd4404836cb218505ccd4b4ad8
TebogoMasete <Tebogomasete16@gmail.com>
2022-07-01 14:41:29 -0700

    Task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -3,24 +3,21 @@
  *main - Entry point
  *Return: Always 0 (Success)
  */
-
 int main(void)
-
 {
-
-		int number;
+	int number;
 
 	for (number = 48; number <= 57; number++)
-					{
-					putchar(number);	
+	{
+		putchar(number);
 	if (number == 57)
-					{
+	{
 	break;
-					}
-					putchar(',');
-					putchar (' ');
-					}
-		putchar('\n');	
+	}
+		putchar(',');
+		putchar (' ');
+	}
+		putchar('\n');
 	return (0);
 
 }
