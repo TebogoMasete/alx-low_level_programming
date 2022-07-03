dc05735299714e292a77fcd24162d1ae04119070
TebogoMasete <Tebogomasete16@gmail.com>
2022-07-03 13:16:25 -0700

    Task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,42 +1,35 @@
-#include <stdiob.h>
-#include <time.h>
 #include <stdio.h>
+
 /**
- *main - prints all Possible different combinations of 3 digits.
+ *main - Prints 3 combination of numbers
  *Return: Always 0 (Success)
  */
+
 int main(void)
-
 {
-	int digit1, digit2, digit3;
 
-	for (digit1 = 48, digit2 = 49, digit3 = 50;)
-	{
-	while  ((digit1 < 56) && (digit2 < 57) && (digit3 < 58))
-	{
-		putchar(1);
-		putchar(2);
-		putchar(3);
-	if ((digit1 == 55) && (digit2 == 56) && (digit3 == 57))
-	{
-		putchar('\n');
-		digit1++;
-		digit2++;
-		digit3++;
-	{
-	else
-	}	putchar(44);
-		putchar(32);
-	if (digit3 < 57)
-		digit3++;
-	}
-	else if ((digit2 < 56) && (digit3 == 57))
-		digit3 = 1 + ++digit2;
-	}
-	else
-		digit2 = 1 + ++digit1;
-		digit3 = 1 + digit2;
-	}
-	putchar('\n');
-	return (0);
+int c, i, k;
+
+for (c = '0'; c <= '9'; c++)
+{
+for (i = '0'; i <= '9'; i++)
+{
+for (k = '0'; k <= '9'; k++)
+{
+if (c < i && i < k)
+{
+	putchar(c);
+	putchar(i);
+	putchar(k);
+if (c != '7')
+{
+	putchar(',');
+	putchar(' ');
+}
+}
+}
+}
+}
+putchar('\n');
+return (0);
 }
