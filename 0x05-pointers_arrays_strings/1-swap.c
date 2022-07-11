1d1972d8f3a3b36f266ace4c16203032ce65d4f9
TebogoMasete <Tebogomasete16@gmail.com>
2022-07-11 11:15:23 -0700

    Task 1

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,5 +1,15 @@
 #include "main.h"
-#include <studio.h>
 /**
- * swap_int: swap the value of two intigers
+ * swap_int - swap the value of two intigers
+ * @a: intiger a
+ * @b: intiger b
  * Return: Always 0
+ */
+void swap_int(int *a, int *b)
+{
+	int c;
+
+	c = *a;
+	*a = *b;
+	*b = c;
+}
