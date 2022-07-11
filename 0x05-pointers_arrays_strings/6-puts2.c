8aa6ed54b8d23aa558952559a58a1701ff9d3676
TebogoMasete <Tebogomasete16@gmail.com>
2022-07-11 10:51:40 -0700

    Task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * puts2 - prints every other character of a string
+ * @str: Set of characters
+ * Return: Nothing
+ */
+void puts2(char *str)
+{
+	int count = 0;
+
+	while (count >= 0)
+	{
+		if (str[count] == '\0')
+		{
+			_putchar('\n');
+			break;
+		}
+		if (count % 2 == 0)
+			_putchar(str[count]);
+		count++;
+	}
+}
